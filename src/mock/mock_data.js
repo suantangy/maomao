1b4243591cd3329a0e163ee7dd65de604ce2f38d
suantangy <113564519+suantangy@users.noreply.github.com>
2026-07-17 06:12:06 +0800

    chore: 更新导航数据 - 2026/7/17 06:12:06

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -164,13 +164,6 @@ export const mockData = {
           "icon": "https://tu.apao.kdns.fr/1784051795824-7e45b847-862d-4017-b7c2-add5748d7a03.jpg"
         },
         {
-          "id": "stackoverflow",
-          "name": "临时邮箱",
-          "url": "https://temporary-email.org",
-          "description": "",
-          "icon": "https://temporary-email.org/uploads/logo.png"
-        },
-        {
           "id": "linuxdo",
           "name": "玩偶监控",
           "url": "https://pan-site-monitor.douer.me",
@@ -178,18 +171,18 @@ export const mockData = {
           "icon": "https://tu.apao.kdns.fr/1784053505712-15aff263-b481-4526-a263-2d73f4330b6a.jpg"
         },
         {
+          "id": "stackoverflow",
+          "name": "临时邮箱",
+          "url": "https://temporary-email.org",
+          "description": "",
+          "icon": "https://temporary-email.org/uploads/logo.png"
+        },
+        {
           "id": "52pojie",
           "name": "代理加速",
           "url": "https://proxy.dhsv.eu.org/",
           "description": "",
-          "icon": "https://tu.apao.kdns.fr/img/png/1784056739140-9ddeec62-b6f1-4bd9-970e-2f716cd5fa87.png"
-        },
-        {
-          "id": "site-1784072468046",
-          "name": "接口解密",
-          "url": "https://jm.ctv.dpdns.org",
-          "description": "",
-          "icon": "https://tu.apao.kdns.fr/img/png/1784072896082-01bc8d9a-181f-4ed7-9ce6-7eb6c584ae88.png"
+          "icon": "https://images.suantangyu.cc.cd/file/1784239901656_IMG_20260715_031813.png"
         },
         {
           "id": "v2ex",
@@ -199,11 +192,18 @@ export const mockData = {
           "icon": "https://tu.apao.kdns.fr/img/jpg/1784057128506-7d8645b9-69a8-4804-b1a7-27f905b10db4.jpg"
         },
         {
-          "id": "site-1784238850075",
+          "id": "site-1784072468046",
+          "name": "接口解密",
+          "url": "https://jm.ctv.dpdns.org",
+          "description": "",
+          "icon": "https://tu.apao.kdns.fr/img/png/1784072896082-01bc8d9a-181f-4ed7-9ce6-7eb6c584ae88.png"
+        },
+        {
+          "id": "site-1784239848212",
           "name": "短视频去水印",
           "url": "http://www.kaniptv.cn/%E5%8E%BB%E6%B0%B4%E5%8D%B0/",
           "description": "",
-          "icon": "https://www.faviconextractor.com/favicon/www.kaniptv.cn"
+          "icon": "https://images.suantangyu.cc.cd/file/1784239833183_IMG_20260716_222254.png"
         }
       ]
     },
